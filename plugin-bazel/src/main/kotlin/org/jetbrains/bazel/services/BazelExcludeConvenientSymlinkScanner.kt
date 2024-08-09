e6abce82a72e71ae3814009700705293253a97b4
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-08-09 20:36:10 +0200

    update

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/services/BazelExcludeConvenientSymlinkScanner.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/services/BazelExcludeConvenientSymlinkScanner.kt
@@ -1,26 +1,44 @@
 package org.jetbrains.bazel.services
 
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.isFile
+import com.intellij.platform.backend.workspace.WorkspaceModel
+import com.intellij.platform.backend.workspace.toVirtualFileUrl
+import com.intellij.platform.workspace.jps.entities.ExcludeUrlEntity
 import com.intellij.util.indexing.roots.IndexableFileScanner
 import com.intellij.util.indexing.roots.kind.ContentOrigin
 import com.intellij.util.indexing.roots.kind.ProjectFileOrDirOrigin
 import org.apache.commons.io.FileUtils.isSymlink
 import org.jetbrains.plugins.bsp.config.isBspProject
 import org.jetbrains.plugins.bsp.config.rootDir
+import org.jetbrains.plugins.bsp.workspacemodel.entities.BspEntitySource
 
 class BazelExcludeConvenientSymlinkScanner: IndexableFileScanner {
   override fun startSession(project: Project): IndexableFileScanner.ScanSession {
     return IndexableFileScanner.ScanSession {
       if (it is ContentOrigin || it is ProjectFileOrDirOrigin) {
-        IndexableFileScanner.IndexableFileVisitor { fileOrDir -> dummyVisitor(fileOrDir, project) }
+        IndexableFileScanner.IndexableFileVisitor { fileOrDir -> excludeUrl(fileOrDir, project) }
       } else null
     }
   }
 
-  private fun dummyVisitor(fileOrDir: VirtualFile, project: Project): Boolean? =
-    fileOrDir.shouldExclude(project).takeIf { it }
+  private fun excludeUrl(fileOrDir: VirtualFile, project: Project) {
+    if (fileOrDir.shouldExclude(project)) {
+      val workspaceModel = WorkspaceModel.getInstance(project)
+      runWriteAction {
+        workspaceModel.updateProjectModel("Exclude file: ${fileOrDir.path}") { storage ->
+          storage.addEntity(
+            ExcludeUrlEntity(
+              url = fileOrDir.toVirtualFileUrl(workspaceModel.getVirtualFileUrlManager()),
+              entitySource = BspEntitySource
+            )
+          )
+        }
+      }
+    }
+  }
 
   private fun VirtualFile.shouldExclude(project: Project) = when {
     !project.isBspProject -> false
